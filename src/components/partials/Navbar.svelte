8f292d7c4635dc2b4aef49cae855562b9a5e813c
Ahmad Habibi <ahmadhabibi7159@gmail.com>
2024-11-18 03:31:17 +0800

    [chore] init project

---
+++ src/components/partials/Navbar.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+
+</script>
+
+<header class="fixed flex justify-center items-center w-full border-b-4 h-20 border-zinc-950 top-0 z-40 bg-emerald-300 px-6">
+  <div class="flex flex-row justify-between items-center gap-4 mx-auto w-9/12">
+    <a href="/">Remove BG</a>
+    <nav class="flex flex-row gap-5 items-center">
+      <a href="/" class="inline-flex justify-center items-center py-2 px-5 border-zinc-950 neoshadow">
+        Home
+      </a>
+      <a href="/remove" class="inline-flex justify-center items-center py-2 px-5 border-zinc-950 neoshadow">
+        Remove
+      </a>
+    </nav>
+  </div>
+</header>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import '../app.css';
+	import Navbar from '../components/partials/Navbar.svelte';
+
+	let { children } = $props();
+</script>
+
+<main class="main-container">
+	<div class="h-full w-full flex">
+		<Navbar />
+		<div class="content">
+			{@render children()}
+		</div>
+	</div>
+</main>
+
+<style lang="postcss">
+	.main-container {
+		@apply bg-white text-zinc-800 top-0 bottom-0 left-0 right-0 h-screen w-screen;
+	}
+	.root-content {
+		-webkit-box-orient: vertical;
+		-webkit-box-direction: normal;
+	}
+
+	.content {
+		@apply bg-zinc-100 min-h-[90dvh] max-h-fit w-full px-4 md:px-10 md:py-6 py-4;
+	}
+</style>
